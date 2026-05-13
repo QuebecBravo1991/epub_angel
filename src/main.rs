5c506db05c0a316093d0504e48cad39f83ad83cf
Quade Barnes <quade.barnes@gmail.com>
2026-05-13 08:07:28 -0600

    Added output name generation/parsing.

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,22 @@ struct Args {
     language: Option<String>,
 }
 
-fn unzip(input: &String) {}
+fn unzip(input_name: &String) {}
 
 fn clean(lang: &Option<String>) {}
 
-fn rezip(output: &Option<String>) {}
+fn rezip(output_name: &Option<String>, input_name: &String) -> String {
+    let default = format!(
+        "{}_cleaned.epub",
+        input_name.strip_suffix(".epub").unwrap_or(input_name)
+    );
+    return output_name.as_deref().unwrap_or(&default).to_string();
+}
 
 fn main() {
     let args = Args::parse();
 
     unzip(&args.input);
     clean(&args.language);
-    rezip(&args.output);
+    rezip(&args.output, &args.input);
 }
