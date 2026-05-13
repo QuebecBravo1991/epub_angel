5a9bd4edd91f8439962c07120ac4094b3529c277
Quade Barnes <quade.barnes@gmail.com>
2026-05-12 18:37:35 -0600

    Add clap dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// The path to the file to be cleaned.
+    #[arg(short, long)]
+    input: String,
+
+    /// The name of the cleaned e-book.
+    #[arg(short, long)]
+    output: Option<String>,
+
+    /// The language of the input e-book.
+    #[arg(short, long)]
+    language: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:#?}", args)
 }
